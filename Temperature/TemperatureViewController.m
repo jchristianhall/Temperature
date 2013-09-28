e845b6706bb0ef0824127b629713ff8af3b79d8c
Christian Hall <jchristian192@gmail.com>
2013-09-28 16:26:19 -0500

    Initial commit. Adds a UILabel and UiStepper with corresponding actions to simulate a temperature adjuster. Linear interpolation used to change label colour.

--- Temperature/TemperatureViewController.m
+++ Temperature/TemperatureViewController.m
@@ -10,6 +10,9 @@
 
 @interface TemperatureViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+- (IBAction)valueChanged:(UIStepper *)sender;
+
 @end
 
 @implementation TemperatureViewController
@@ -24,6 +27,24 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)valueChanged:(UIStepper *)sender
+{
+    double value = [sender value];
+    if (value >= 50 && value <= 70)
+    {
+        double adjustedBlue = (value - 70)/(50-70);
+        UIColor *blue = [UIColor colorWithRed:0 green:0 blue:adjustedBlue alpha:1];
+        [self.label setTextColor:blue];
+    }
+    else if (value <= 90 && value > 70)
+    {
+        double adjustedRed = (value - 70)/(90-70);
+        UIColor *red = [UIColor colorWithRed:adjustedRed green:0 blue:0 alpha:1];
+        [self.label setTextColor:red];
+    }
+    [self.label setText:[NSString stringWithFormat:@"%d", (int)value]];
 }
 
 @end
